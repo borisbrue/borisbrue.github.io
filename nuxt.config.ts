29be185dac3ccdaf6c7cb3fb75accef42fe7d63f
Boris Brue <boris@zuckersalzundpfeffer.de>
2023-11-24 21:34:25 +0100

    some updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
+  head: {
+    title: 'Boris Brue | Full Stack Developer',
+    htmlAttrs: {
+      lang: 'de'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+  ],
   nitro: {
-    serveStatic: true,
+
   },
   app: {
     baseURL: '/borisbrue.github.io/', // baseURL: '/<repository>/'
