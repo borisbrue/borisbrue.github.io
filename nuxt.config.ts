28f33e94cfed01a3cde5c4584cc409df8655b34f
Boris Brue <boris@zuckersalzundpfeffer.de>
2023-11-24 20:23:37 +0100

    some updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: '/borisbrue.github.io/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss'
