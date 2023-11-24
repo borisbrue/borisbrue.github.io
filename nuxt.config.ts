44022fec7ddab0c4c9fd49a302c0e57974d22b5c
Boris Brue <boris@zuckersalzundpfeffer.de>
2023-11-24 20:54:24 +0100

    some updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    serveStatic: true,
+  },
   app: {
     baseURL: '/borisbrue.github.io/', // baseURL: '/<repository>/'
     buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
