426c42e0f6bdb9e15a849fb2453d49542fcbb4a8
borisbrue <boris@zuckersalzundpfeffer.de>
2026-02-10 08:59:25 +0100

    fix: update Tailwind configuration to include content paths for better purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  //...
+  content: [
+		'./components/**/*.{js,vue,ts}',
+		'./layouts/**/*.vue',
+		'./pages/**/*.vue',
+		'./app.vue'
+	],
+	// The rest of your code, if you have any
   theme: {
     fontFamily: {
       display: "Source Code Pro",
